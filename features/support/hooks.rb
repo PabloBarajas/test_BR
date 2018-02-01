7b737a6f5e95ecfa2fe18bd38d3c5f46c1d13c87
pablobarajas <pdbarajas@hotmail.com>
2018-02-02 00:00:37 +0100

    Added browser configuration in hooks.rb.

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,8 +1,24 @@
 require 'watir'
+require 'yaml'
 
 Before do |scenario|
   DataMagic.load_for_scenario(scenario)
-  @browser = Watir::Browser.new :chrome
+
+  browser = ENV['browser']
+  case browser
+    when 'firefox'
+      @browser = Watir::Browser.new :firefox
+    when 'crome'
+      @browser = Watir::Browser.new :chrome
+    when 'ie'
+      @browser = Watir::Browser.new :ie
+    when 'edge'
+      @browser = Watir::Browser.new :edge
+    when 'safari'
+      @browser = Watir::Browser.new :safari
+    else
+      @browser = Watir::Browser.new :chrome
+  end
 end
 
 
